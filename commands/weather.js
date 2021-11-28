5829a66afec588c35992efdfc5cf50252458de33
FordFriedel <ford.friedel@gmail.com>
2021-11-28 00:46:45 -0600

    weather function creation

--- commands/weather.js
+++ commands/weather.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch');
 
 exports.run = async (client, message, args) => {
     let res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${args}&appid=${client.config.weather_token}`);
+    let jsonData = await  res.json();
+    console.log(jsonData);
 
 
 
