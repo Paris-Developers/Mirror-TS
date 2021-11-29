d8d4adc63fccee8a03d84a2c5720fe9e8167560c
FordFriedel <ford.friedel@gmail.com>
2021-11-28 18:37:03 -0600

    weather emoji update

--- commands/weather.js
+++ commands/weather.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const {MessageEmbed} = require('discord.js');
 const { find } = require('geo-tz');
 
-const weatherEmoji = {"Rain":":cloud_rain:","Thunderstorm":":thunder_cloud_rain:","Drizzle":":cloud_rain:", "Snow":":cloud_snow:", "Clear":":sunny:", "Clouds":":cloud:"}
+const weatherEmoji = {"Rain":":cloud_rain:","Thunderstorm":":thunder_cloud_rain:","Drizzle":":cloud_rain:","Snow":":cloud_snow:", "Clear":":sunny:", "Clouds":":cloud:"}
 const cardinalDir = {0:"N",1:"NNE",2:"NE",3:"ENE",4:"E",5:"ESE",6:"SE",7:"SSE",8:"S",9:"SSW",10:"SW",11:"WSW",12:"W",13:"WNW",14:"NW",15:"NNW",16:"N"}
 
 exports.run = async (client, message, args) => {
