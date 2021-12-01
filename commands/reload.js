6e320ae09923aed05acbd86b57fe2f233542104a
Zacary Halasz <halaszkid@gmail.com>
2021-12-01 00:38:33 -0600

    another code block isn't the easiest way

--- commands/reload.js
+++ commands/reload.js
@@ -2,25 +2,18 @@ exports.run = (client, message, args) => {
     if (message.author.id == client.config.owner) {
         if(!args || args.size < 1) return message.reply("Must provide a command/keyword name to reload.");
         const commandName = args[0];
-        // Check if the command exists and is valid
-        if(client.commands.has(commandName)) {
-            // the path is relative to the *current folder*, so just ./filename.js
-            delete require.cache[require.resolve(`./${commandName}.js`)];
-            // We also need to delete and reload the command from the client.commands Enmap
-            client.commands.delete(commandName);
-            const props = require(`./${commandName}.js`);
-            client.commands.set(commandName, props);
-            message.reply(`The command ${commandName} has been reloaded`);
-        } else if (client.keywords.has(commandName)) { //we should also check keywords, but since it uses a different enmap, another code block is (probably) the easiest way
-            // the path is relative to the *current folder*, so remap to keywords
-            delete require.cache[require.resolve(`../keywords/${commandName}.js`)];
-            // We also need to delete and reload the keyword from the client.keywords Enmap
-            client.keywords.delete(commandName);
-            const props = require(`../keywords/${commandName}.js`);
-            client.keywords.set(commandName, props);
-            message.reply(`The keyword ${commandName} has been reloaded`);
-        } else {
-            message.reply(`Could not find command/keyword ${commandName}`);
+
+        if (!client.commands.has(commandName) && !client.keywords.has(commandName)) { //neither enmap has it, not valid
+            return message.reply(`Could not find command/keyword ${commandName}`);
         }
+
+        const isCommand = client.commands.has(commandName); //whether we're dealing with a command or keyword
+        const pathToFile = isCommand ? `./${commandName}.js` : `../keywords/${commandName}.js` // if it's a keyword we need to remap to keyword directory
+        delete require.cache[require.resolve(pathToFile)]; //remove from cache
+        const enmapName = isCommand ? 'commands' : 'keywords'; //we need to use the correct enmap
+        client[enmapName].delete(commandName); //remove the current version
+        const props = require(pathToFile); //get the new version
+        client[enmapName].set(commandName, props); //set it to the correct enmap
+        message.reply(`Reloaded the ${enmapName.slice(0, -1)} ${commandName}`);
     }
 }
