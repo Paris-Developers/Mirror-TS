3ffa20b558357bb46c93f6005c211c41655a7422
FordFriedel <ford.friedel@gmail.com>
2021-12-01 13:35:14 -0600

    commit

--- commands/news.js
+++ commands/news.js
@@ -9,11 +9,13 @@ exports.run = async (client,message,args) => {
     // determine if message was empty and then pulls data from the API and stores it into a JSON object
     let res = '';
     if(args.length == 0) {
-        res = await fetch(`https://newsapi.org/v2/top-headlines?&from=${today}&sortBy=publishedAt&apiKey=${news_token}`);
+        res = await fetch(`https://newsapi.org/v2/top-headlines?&from=${today}&sortBy=publishedAt&apiKey=${client.config.news_token}`);
     } else {
-        res = await fetch(`https://newsapi.org/v2/top-headlines?q=${query}&from=${today}&sortBy=publishedAt&apiKey=${news_token}`);
+        res = await fetch(`https://newsapi.org/v2/top-headlines?q=${query}&from=${today}&sortBy=publishedAt&apiKey=${client.config.news_token}`);
     }
     let jsonData = await res.json();
+    console.log(jsonData);
+    console.log('hi');
 
     //figure out what we want to display in our message, do we want an embe
 
