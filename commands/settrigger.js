8f3fd3e7d723c41925c999d084e62d614d7b7735
Zacary Halasz <halaszkid@gmail.com>
2021-12-05 22:12:40 -0600

    more error handling

--- commands/settrigger.js
+++ commands/settrigger.js
@@ -8,13 +8,18 @@ exports.run = (client,message,args,identifiedArgs) => {
     if (!client.commands.get(commandName)) return message.channel.send('Command not found/invalid command');
     const results = chrono.parse(identifiedArgs.t); // chrono parses natural language into datetime
     if (!results[0]) return message.channel.send(`Couldn't parse '${identifiedArgs.t}' into a valid time.\nTry something like: 9:35pm ET`);
-    let datedResult = results[0].start.date(); //get a javascript date object
+    let datedResult = results[0].start.date(); //get a javascript date object (not really needed I guess? could just pull from results directly)
     //set the cronjob for every day using specified hours/minutes
     let job = new CronJob(`${datedResult.getMinutes()} ${datedResult.getHours()} * * *`, () => {
         //we need to split the data in 'a' to an array. If they didn't specify any args pass an empty array 
         let argsToPass = !identifiedArgs.a ? [] : identifiedArgs.a.trim().split(/ +/g); 
         //get the command from the enmap and pass it what it needs
-        client.commands.get(commandName).run(client,message,argsToPass);
+        try {
+            client.commands.get(commandName).run(client,message,argsToPass);
+        } catch(err) {
+            message.channel.send(`Error with trigger ${commandName}: ${err}`);
+            job.stop();
+        }
     });
     job.start();
     //TODO save needed data to creat cronjob in a persitent enmap
