932bc75f5f8436b0ca3d14f588cf7b6193d448e6
Zacary Halasz <halaszkid@gmail.com>
2021-12-05 22:05:27 -0600

    error handling improvements & command name parsing

--- commands/settrigger.js
+++ commands/settrigger.js
@@ -4,15 +4,17 @@ const { CronJob } = require('cron');
 exports.run = (client,message,args,identifiedArgs) => {
     if (!identifiedArgs.t) return message.channel.send('Please specify a time with -t time');
     if (!identifiedArgs.c) return message.channel.send('Please specify a command with -c command');
-    if (!client.commands.get(identifiedArgs.c)) return message.channel.send('Command not found/invalid command');
+    const commandName = identifiedArgs.c.trim().toLowerCase(); //trim it and make sure it's lowercase as all enmap keys are lowecase
+    if (!client.commands.get(commandName)) return message.channel.send('Command not found/invalid command');
     const results = chrono.parse(identifiedArgs.t); // chrono parses natural language into datetime
+    if (!results[0]) return message.channel.send(`Couldn't parse '${identifiedArgs.t}' into a valid time.\nTry something like: 9:35pm ET`);
     let datedResult = results[0].start.date(); //get a javascript date object
     //set the cronjob for every day using specified hours/minutes
     let job = new CronJob(`${datedResult.getMinutes()} ${datedResult.getHours()} * * *`, () => {
         //we need to split the data in 'a' to an array. If they didn't specify any args pass an empty array 
         let argsToPass = !identifiedArgs.a ? [] : identifiedArgs.a.trim().split(/ +/g); 
         //get the command from the enmap and pass it what it needs
-        client.commands.get(identifiedArgs.c).run(client,message,argsToPass);
+        client.commands.get(commandName).run(client,message,argsToPass);
     });
     job.start();
     //TODO save needed data to creat cronjob in a persitent enmap
