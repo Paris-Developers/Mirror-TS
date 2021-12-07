42e01942b284a0fa3a511853f8443bcb5c9ceb54
FordFriedel <ford.friedel@gmail.com>
2021-12-07 11:11:31 -0600

    updates to weather fxn

--- commands/news.js
+++ commands/news.js
@@ -2,42 +2,40 @@
 //Unfinished
 //Returns top news story in the US or returns the top story(ies?) from a specified topic
 
-const {MessageEmbed} = require('discord.js');
+const {MessageEmbed, Message} = require('discord.js');
 const fetch = require('node-fetch');
 
 exports.run = async (client,message,args) => {
-
-    console.log('inside the News fxn');
-    let query = args[0]; //finish
-    let today = new Date(); //finish
-
-    // determine if message was empty and then pulls data from the API and stores it into a JSON object
-    //console.log(args);
-
-    let res = '';
-    if(args.length == 0) {//sample code documentation for testing purposes
-        res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${client.config.news_token}`);
-    } else {
-        res = await fetch(`https://newsapi.org/v2/top-headlines?q=${query}&from=${today}&sortBy=publishedAt&apiKey=${client.config.news_token}`);
+    try{//Runs code as normal, sends  catch if an error is recieved
+        const embed = new MessageEmbed() //creates embed
+            .setColor('#FFFFFF')
+            .setTimestamp();
+        if(args.length==0){//fetches the API without query
+            res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${client.config.news_token}`);
+            embed.setFooter('Top news story in the US right now');
+        }
+        else{//when there is a query requested run the block behind
+            let query = '';
+            for(let arg in args){
+                query += `${arg}+`;
+            }
+            res = await fetch(`https://newsapi.org/v2/top-headlines?country-us&q=${query}&apiKey=${client.config.news_token}`);
+        }
+        //sets data structure for the API pull
+        let jsonData = await res.json();
+        
+        //specify embed modifications for individual option and sends message
+        embed.setTitle(`${jsonData.articles[0].title}`);
+        embed.setURL(`${jsonData.articles[0].url}`);
+        embed.setDescription(`${jsonData.articles[0].description}`);
+        embed.setThumbnail(`${jsonData.articles[0].urlToImage}`);
+        message.channel.send({embeds:[embed]});
     }
-    let jsonData = await res.json();
-
-    //figure out what we want to display in our message, do we want an embe
-
-    const embed = new MessageEmbed()
-        .setTitle(`${jsonData.articles[0].title}`)
-        .setURL(`${jsonData.articles[0].url}`)
-        .setDescription(`${jsonData.articles[0].description}`)
+    catch(err){//catches error 
+        //add documentation and finish error testong for api related errors
+        const embed = new MessageEmbed()
         .setColor('#FFFFFF')
-        .setFooter('Top news story in the US right now')
-        .setThumbnail(`${jsonData.articles[0].urlToImage}`)
-        .setTimestamp();
-    
-    message.channel.send({embeds:[embed]})
-    //add embed
-
-    //add messsage send
-
-
+        .setDescription('Error: Try calling the function again');  
+    }
 }
   
