dae8c7c1f486004339373274e73b897c4b681948
Ford Friedel <90861679+FordFriedel@users.noreply.github.com>
2021-12-24 13:33:25 -0600

    Update 🪞.js
    mirror emoji fix

--- keywords/🪞.js
+++ keywords/🪞.js
@@ -1,6 +1,6 @@
 //Supposed to react to a mirror emoji with a mirror emoji
 
-exports.keywordName = 'whatthefuckhowdoitypeamirror';
+exports.keywordName = '🪞';
 
 exports.run = (client, message, args) => {
     message.react('🪞');
