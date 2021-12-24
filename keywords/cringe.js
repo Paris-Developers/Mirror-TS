db45d222e1f8a1820e1718c0b2bc494c7506f230
FordFriedel <ford.friedel@gmail.com>
2021-12-24 13:42:43 -0600

    check

---
+++ keywords/cringe.js
@@ -0,0 +1,5 @@
+exports.keywordName = 'cringe';
+
+exports.run = (client, message, args) => {
+    message.reply(`https://tenor.com/view/dies-of-cringe-cringe-gif-20747133`);
+}
---
+++ keywords/fortbush.js
@@ -0,0 +1,9 @@
+//test reaction to a custom emoji which we cannot make a file name for
+//not working for some reason smile
+exports.keywordName = '<:fortbush:816549663812485151>'
+
+
+
+exports.run = (client, message, args) => {
+    message.react(':FortBush:816549663812485151');
+}
