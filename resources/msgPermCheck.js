d4f5d2de6fe2a2c7a7627e095a5d62aed4637175
FordFriedel <ford.friedel@gmail.com>
2022-01-13 21:50:44 -0600

    fixes permissionsCheck and messagePermsCheck

--- resources/msgPermCheck.js
+++ resources/msgPermCheck.js
@@ -3,9 +3,9 @@
 exports.msgPermsCheck = async (client,message,permissionsToCheck) => {
     let guildMember = await message.guild.members.fetch(client.user);
     let permissions = message.channel.permissionsFor(guildMember);
-    permissionsToCheck.forEach(async (permission) => {
+    for(let permission of permissionsToCheck){
         if(!permissions.has(permission)) return false;
-    })
+    }
     return true;
 }
 //Checks a single permission for a messages channel, returns a boolean
