4590869dda325e8b129ea2ad4287c9072ff74f0c
FordFriedel <ford.friedel@gmail.com>
2022-02-10 10:35:38 -0600

    support and invite commands

---
+++ commands/support.js
@@ -0,0 +1,21 @@
+const { Permissions } = require('discord.js');
+
+exports.commandName = 'support';
+
+exports.run = async (client, interaction) => {
+    if(!(await client.permissionsCheck(client,interaction,[Permissions.FLAGS.SEND_MESSAGES,Permissions.FLAGS.EMBED_LINKS]))){
+        console.log(`Missing permissions to use ${this.commandName} in channel: ${interaction.channel.name}, in ${interaction.guild.name}`);
+        return;
+    }
+    interaction.reply('discord.gg/uvdg2R5PAU');
+    return;
+
+    //TODO Error Handling
+}
+
+exports.registerData = (client) => {
+    return {
+        name: this.commandName,
+        description: 'Join Mirrors public support server'
+    }
+};
