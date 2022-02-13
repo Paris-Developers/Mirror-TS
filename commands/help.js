0d041f3e4fa8465d3bee3dad8302b137b74c1979
FordFriedel <ford.friedel@gmail.com>
2022-02-13 14:48:25 -0600

    temp fix to help

--- commands/help.js
+++ commands/help.js
@@ -9,9 +9,11 @@ exports.run = async (client,interaction) => {
     const page1 = new MessageEmbed()
         .setColor('#FFFFFF')
         .setTitle(':mirror: **__Mirror__**')
-        .setDescription('Discord utility bot created by Ford, Zac, Leo (not really) and Gavin')
-        .addFields({name: '__Command List:__', value: '**Page 1:** Voice Commands\n**Page 2:** Informative Commands\n**Page 3:** Other Commands', inline: false})
-        .setFooter('Page 1 of 2');
+        .setDescription('Discord utility bot created by Ford, Zac, and Marty')
+        .addFields(
+            {name: '__Support server:__', value: 'Interested in contributing or learning about development? Join our [dev server](https://discord.gg/uvdg2R5PAU)', inline: false},
+            {name: '__Command List:__', value: '**Page 1:** Voice Commands\n**Page 2:** Informative Commands\n**Page 3:** Other Commands', inline: false})
+        .setFooter('Page 1 of 4');
     const page2 = new MessageEmbed()
         .setColor('#FFFFFF')
         .setTitle(':sound: **__Voice Commands__**')
@@ -21,32 +23,27 @@ exports.run = async (client,interaction) => {
            '/intro: Have Mirror set a specified youtube video as your intro theme.\n'+
            '/banintro: Allows an administrator to remove a specified users intro.\n'+
            '/sicko: :skull:')
-        .setFooter('Page 2 of 2');
-    // const page3 = new MessageEmbed()
-    //     .setColor('#FFFFFF')
-    //     .setTitle('**__Informative Commands__**')
-
-    //     //.setTimestamp()
-    //     .setDescription(
-    //     '/weather: \n'+
-    //     '/stock: \n'+
-    //     '/news: .\n'+
-    //     '/: \n'+
-    //     '/:')
-    //     .addField()
-    //     //.setFooter();
-    // const page4 = new MessageEmbed()
-    //     .setColor('#FFFFFF')
-    //     .setTitle('**__Other Commands__**')
-    //     .setTimestamp()
-    //     .setDescription(
-    //     '/poll: \n'+
-    //     '/kanye: \n'+
-    //     '/: .\n'+
-    //     '/: \n'+
-    //     '/:');
-    //     //.addField()
-    //     //.setFooter();
+        .setFooter('Page 2 of 4');
+    const page3 = new MessageEmbed()
+        .setColor('#FFFFFF')
+        .setTitle(':newspaper: **__Informative Commands__**')
+        .setDescription(
+            '/weather: Displays the current weather for a specified city. \n'+
+            '/stock: Displays daily reports for up to 10 specified stocks. \n'+
+            '/nasa: Sends the Nasa Astrology Picture of the Day.\n'+
+            '/news: WIP.\n'+
+            '/github: Links the open source code for Mirror' )
+        .setFooter('Page 3 of 4');
+    const page4 = new MessageEmbed()
+        .setColor('#FFFFFF')
+        .setTitle(':bell: **__Other Commands__**')
+        .setDescription(
+        '/poll: Create a timed poll with up to 10 options  \n'+
+        '/kanye: Sends a random kanye quote\n'+
+        '/kawaii: Sends a cute catgirl.\n'+
+        '/birthday: Set your birthdayfor a message on your special day. \n'+
+        '/mirror: :mirror:')
+        .setFooter('Page 4 of 4');
     // const page5 = new MessageEmbed()
     //     .setColor('#FFFFFF');
     //     //.setTitle()s
@@ -55,7 +52,7 @@ exports.run = async (client,interaction) => {
     //     //.setDescription()
     //     //.addField()
     //     //.setFooter();
-    let embedArray = [page1,page2];
+    let embedArray = [page1,page2,page3,page4];
     let index = 0;
     let message = await interaction.reply({embeds: [embedArray[index]], fetchReply: true}); //fetch the reply and store it so we can react to it and use it in the collector
     await message.react('⏪');
