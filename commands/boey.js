5057deba3e7dcfa866f4dd858871e4b6b6f5696d
FordFriedel <ford.friedel@gmail.com>
2022-02-14 17:51:02 -0600

    adds functionality to boey.js

---
+++ commands/boey.js
@@ -0,0 +1,23 @@
+const { MessageEmbed, User } = require('discord.js');
+
+exports.commandName = 'boey';
+
+exports.registerData = (client) => {
+    return {
+        name: this.commandName,
+        description: 'Sends Boeys stream information!'
+    }
+}
+
+exports.run = async (client,interaction) => {
+    let boey = await client.users.fetch('331511682155282432');
+    console.log(boey);
+    const embed = new MessageEmbed()
+        .setColor('#6441a5')
+        .setTitle(':otter: **__BOEY STREAM HERE__**')
+        .setURL('https://www.twitch.tv/kanganya')
+        .setDescription('  \n:goat: Come watch mamas slay the competition <:power_cry:759835413296316496>')
+        .setThumbnail(boey.displayAvatarURL());
+    interaction.reply({embeds:[embed]});
+    return;
+}
