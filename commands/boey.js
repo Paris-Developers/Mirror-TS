895b21de34286453a6b6c12e5abea91b97a63009
FordFriedel <ford.friedel@gmail.com>
2022-02-14 17:56:43 -0600

    removes unneeded log

--- commands/boey.js
+++ commands/boey.js
@@ -10,13 +10,13 @@ exports.registerData = (client) => {
 }
 
 exports.run = async (client,interaction) => {
+    //TODO: Permission Check
     let boey = await client.users.fetch('331511682155282432');
-    console.log(boey);
     const embed = new MessageEmbed()
         .setColor('#6441a5')
         .setTitle(':otter: **__BOEY STREAM HERE__**')
         .setURL('https://www.twitch.tv/kanganya')
-        .setDescription('  \n:goat: Come watch mamas slay the competition <:power_cry:759835413296316496>')
+        .setDescription(':goat: Come watch mamas slay the competition <:power_cry:759835413296316496>')
         .setThumbnail(boey.displayAvatarURL());
     interaction.reply({embeds:[embed]});
     return;
