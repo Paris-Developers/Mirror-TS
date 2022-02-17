5bb342d29fcea25b1df8ff58d569f1fa07671b74
Zacary Halasz <halaszkid@gmail.com>
2022-02-17 03:13:07 -0600

    prettier interactionCreate

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,11 +1,10 @@
-import { CommandInteraction } from "discord.js";
-import { Bot } from "../bot";
+import { CommandInteraction } from 'discord.js';
+import { Bot } from '../bot';
 
 module.exports = async (bot: Bot, interaction: CommandInteraction) => {
-    if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-    if (!bot.commands.has(interaction.commandName)) return;
+  if (!bot.commands.has(interaction.commandName)) return;
 
-    bot.commands.get(interaction.commandName).run(bot, interaction);
-
-}
+  bot.commands.get(interaction.commandName).run(bot, interaction);
+};
