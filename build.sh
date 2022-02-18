7634d49bb9885eb9976bf645fd0d94fb7c323747
Zacary Halasz <halaszkid@gmail.com>
2022-02-17 19:34:23 -0600

    append test

--- build.sh
+++ build.sh
@@ -10,10 +10,13 @@ echo "Compiling slash commands into SlashCommands.ts"
 cd src/slashcommands
 echo "//Autobuilt by build.sh" > SlashCommands.ts
 
-
+#Import all the files
 for f in "./"/*
 do
     filename=$(basename $f)
     importName=${filename%.*}
     echo $importName
+    if [importName -ne "SlashCommands"] then
+        printf "import { %s } from './%s';" $importName >> SlashCommands.ts
+    fi
 done
