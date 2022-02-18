488c0f11dc463772e93735b2dd1d5f88946548eb
Zacary Halasz <halaszkid@gmail.com>
2022-02-17 20:07:08 -0600

    change type?

--- src/slashcommands/SlashCommand.ts
+++ src/slashcommands/SlashCommand.ts
@@ -1,13 +1,14 @@
 //All slash commands derive from this interface
 import { Bot } from '../Bot';
 import {
+	ApplicationCommandDataResolvable,
 	ChatInputApplicationCommandData,
 	CommandInteraction,
 } from 'discord.js';
 
 export interface SlashCommand {
 	name: string;
-	registerData: ChatInputApplicationCommandData;
+	registerData: ApplicationCommandDataResolvable;
 	requiredPermissions: Array<bigint>;
 	run(bot: Bot, interaction: CommandInteraction): Promise<void>;
 }
