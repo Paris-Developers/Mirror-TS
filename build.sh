fb2c7b23341531efbf1e07290056fc13884504c2
Zacary Halasz <halaszkid@gmail.com>
2022-02-17 19:43:50 -0600

    full slashcommand test

--- build.sh
+++ build.sh
@@ -16,7 +16,19 @@ do
     filename=$(basename $f)
     importName=${filename%.*}
     if [ "$importName" != "SlashCommands" ]; then
-        echo $importName
-        printf "import { %s } from './%s';" $importName >> SlashCommands.ts
+        printf "import { %s } from './%s';\n" $importName >> SlashCommands.ts
     fi
 done
+#Construct array
+printf "\n ">> SlashCommands.ts
+printf "export let SlashCommands: Array<SlashCommand> = [" >> SlashCommands.ts
+for f in "./"/*
+do
+    filename=$(basename $f)
+    importName=${filename%.*}
+    if [ "$importName" != "SlashCommands" ] && [ "$importName" != "SlashCommand" ]; then
+        printf "    new %s()," %importName>> SlashCommands.ts
+    fi
+done
+printf "];" >> SlashCommands.ts
+cd ../..
