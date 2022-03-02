be7a4977b58d1ee1c3da164bb76805852de1c356
FordFriedel <ford.friedel@gmail.com>
2022-03-02 00:13:26 -0600

    concise subcommands

--- src/slashcommands/Birthday.ts
+++ src/slashcommands/Birthday.ts
@@ -99,6 +99,7 @@ const timezoneCode = {
 	gmt: -1,
 	pst: -2,
 } as timeIndex;
+
 export let bdayDates = new Enmap({ name: 'bdayDates' });
 export let bdayChannels = new Enmap({ name: 'bdayChannels' });
 export let bdayTimes = new Enmap({ name: 'bdayTimes' });
@@ -132,8 +133,9 @@ export class Birthday implements SlashCommand {
 				],
 			},
 			{
-				name: 'channel',
-				description: 'Set the channel where the birthday messages are sent to',
+				name: 'config',
+				description:
+					'Allows an admin to configure the time and channel to send the birthday messages',
 				type: 1,
 				required: false,
 				options: [
@@ -144,21 +146,6 @@ export class Birthday implements SlashCommand {
 						required: true,
 						type: ApplicationCommandOptionTypes.CHANNEL,
 					},
-				],
-			},
-			{
-				name: 'message',
-				description: 'Edit the message used when its someones birthday',
-				type: 1,
-				required: false,
-			},
-			{
-				name: 'time',
-				description:
-					'Edit the time the birthday message is sent in your local time',
-				type: 1,
-				require: false,
-				options: [
 					{
 						name: 'hour',
 						description:
@@ -207,6 +194,55 @@ export class Birthday implements SlashCommand {
 			});
 			return;
 		}
+		if (interaction.options.getSubcommand() == 'config') {
+			//TODO: Verify user is an admin
+			//TODO: Permissions check
+
+			//CHANNEL BLOCK
+			var guildChannel = bdayChannels.ensure(interaction.guild!.id, '');
+			guildChannel = interaction.options.getChannel('channel');
+			bdayChannels.set(interaction.guild!.id, guildChannel.id);
+			bdayChannels.forEach((element) => console.log(element));
+
+			let hour = interaction.options.getInteger('hour')!;
+			if (hour > 23 || hour < 0)
+				return interaction.reply({
+					content:
+						'Invalid hour, please use military format (0-23) where 0 represents midnight.',
+					ephemeral: true,
+				});
+			let minute = interaction.options.getInteger('minute')!;
+			if (minute > 60 || minute < 0)
+				return interaction.reply({
+					content: 'Invalid minute, please provide an integer between 0 and 60',
+					ephemeral: true,
+				});
+			let timezone = interaction.options.getString('timezone')!;
+			let tzChange = timezoneCode[timezone];
+			let cst = hour + tzChange;
+			let date = 'x';
+			if (cst + tzChange >= 24) {
+				date = 'plus';
+				cst -= 24;
+			}
+			if (cst + tzChange < 0) {
+				date = 'minus';
+				cst += 24;
+			}
+			let infostring = bdayChannels.ensure(interaction.guild!.id, '');
+			infostring = `${minute}-${hour}-${date}-${timezone}`;
+			bdayTimes.set(interaction.guild!.id, infostring);
+			birthdayTimer(interaction.guild!.id, bot);
+			interaction.reply({
+				content: `Successfully configured your birthday timer to ${hour}:${minute} ${timezone.toUpperCase()} in ${interaction.options.getChannel(
+					'channel'
+				)}`,
+			});
+		}
+	}
+}
+/*
+		return;
 		if (interaction.options.getSubcommand() == 'channel') {
 			//TODO: Permission Check
 			//TODO: verify user is an admin
@@ -253,7 +289,7 @@ export class Birthday implements SlashCommand {
 				cst += 24;
 			}
 			let infostring = bdayChannels.ensure(`${interaction.guild!.id}`, '');
-			infostring = `${minute}-${hour}-${date}-${timezone}`; //readable splitable string that will be used in creating crons MM-HH-DATEMOD-TIMEZONE
+			infostring =  //readable splitable string that will be used in creating crons MM-HH-DATEMOD-TIMEZONE
 			bdayTimes.set(`${interaction.guild!.id}`, infostring);
 			birthdayTimer(`${interaction.guild!.id}`, bot);
 			interaction.reply({ content: 'Sucessfully configured your time' });
@@ -268,3 +304,4 @@ export class Birthday implements SlashCommand {
 		}
 	}
 }
+*/
