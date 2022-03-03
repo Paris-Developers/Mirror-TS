f71fe5c491b5cd7563c938edf7623218e98d2210
FordFriedel <ford.friedel@gmail.com>
2022-03-02 23:17:06 -0600

    the 727 commit

---
+++ src/keywords/SevenTwentySeven.ts
@@ -0,0 +1,28 @@
+//Keyword: pog
+//Reacts to the keyword with JamesChamp
+
+import { Message, Permissions, MessageEmbed } from 'discord.js';
+import { Bot } from '../Bot';
+import { Keyword } from './Keyword';
+
+export class SevenTwentySeven implements Keyword {
+	name: String = '727';
+	requiredPermissions: bigint[] = [
+		Permissions.FLAGS.ADD_REACTIONS,
+		Permissions.FLAGS.USE_EXTERNAL_EMOJIS,
+		Permissions.FLAGS.SEND_MESSAGES,
+		Permissions.FLAGS.EMBED_LINKS,
+	];
+	async run(
+		bot: Bot,
+		message: Message<boolean>,
+		args: String[]
+	): Promise<void> {
+		await message.delete();
+		let embed = new MessageEmbed()
+			.setColor('#ff66aa')
+			.setImage('https://c.tenor.com/zbPLwrk_K44AAAAC/wysi.gif')
+			.setTitle('**__WHEN YOU FUCKING SEE IT__**');
+		message.channel.send({ embeds: [embed] });
+	}
+}
