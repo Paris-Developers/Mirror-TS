873d1f8b51448cae4d17fefae4d9826b59c23a0c
FordFriedel <ford.friedel@gmail.com>
2022-03-04 08:54:34 -0600

    changes cum to nsfw cmd

--- src/messagecommands/Cum.ts
+++ src/messagecommands/Cum.ts
@@ -3,6 +3,8 @@ import { Message, Permissions } from 'discord.js';
 import { Bot } from '../Bot';
 import { MessageCommand } from './MessageCommand';
 
+import { nsfw } from '../slashcommands/Nsfw';
+
 export class Cum implements MessageCommand {
 	name: string = 'cum';
 	requiredPermissions: bigint[] = [
@@ -14,6 +16,7 @@ export class Cum implements MessageCommand {
 		message: Message<boolean>,
 		args: string[]
 	): Promise<void> {
+		if(nsfw.get(message.guild!.id) != 'on') return;
 		let rand = Math.round(100 * Math.random());
 		if (rand == 69) {
 			message.reply('You are the scrumple king');
