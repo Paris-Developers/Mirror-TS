5f63227920d464b9ee310983f06498a6de4186c0
FordFriedel <ford.friedel@gmail.com>
2022-03-06 16:01:16 -0600

    visual update to NSFW

--- src/slashcommands/Nsfw.ts
+++ src/slashcommands/Nsfw.ts
@@ -1,53 +1,67 @@
-import { ApplicationCommandDataResolvable, CommandInteraction, CacheType, ChatInputApplicationCommandData, Permissions, MessageEmbed } from 'discord.js';
+import {
+	ApplicationCommandDataResolvable,
+	CommandInteraction,
+	CacheType,
+	ChatInputApplicationCommandData,
+	Permissions,
+	MessageEmbed,
+} from 'discord.js';
 import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
 import Enmap from 'enmap';
 import { Bot } from '../Bot';
 import { SlashCommand } from './SlashCommand';
 
-const choices = [{
-    name: 'On',
-    value: 'on',
-}, {
-    name: 'Off',
-    value: 'off',
-}]
+const choices = [
+	{
+		name: 'On',
+		value: 'on',
+	},
+	{
+		name: 'Off',
+		value: 'off',
+	},
+];
 
-export let nsfw = new Enmap({ name: 'nsfw'});
+export let nsfw = new Enmap({ name: 'nsfw' });
 
 export class Nsfw implements SlashCommand {
-    name: string = 'nsfw'
-    registerData: ChatInputApplicationCommandData = {
-        name: this.name,
-        description: 'Check your current NSFW setting, or toggle it ON or OFF',
-        options: [{
-            name: 'toggle',
-            description: 'Switch your servers NSFW status to ON or OFF',
-            type: ApplicationCommandOptionTypes.STRING,
-            required: false,
-            choices: choices,
-        }]
-    }
-    requiredPermissions: bigint[] = [
-        Permissions.FLAGS.SEND_MESSAGES,
-    ]
-    async run(bot: Bot, interaction: CommandInteraction<CacheType>): Promise<void> {
-        var setting = nsfw.ensure(interaction.guild!.id, 'off');
-        const embed = new MessageEmbed();
-        if(interaction.options.getString('toggle')=='on'){
-            nsfw.set(interaction.guild!.id, 'on');
-            embed.setDescription('NSFW has been toggled `ON`');
-            interaction.reply({embeds:[embed]});
-            return;
-        }
-        if(interaction.options.getString('toggle')=='off'){
-            nsfw.set(interaction.guild!.id, 'off');
-            embed.setDescription('NSFW has been toggled `OFF`');
-            interaction.reply({embeds: [embed]});
-            return;
-        }
-        embed.setDescription(`Your NSFW settings are currently set to ${setting}`)
-        interaction.reply({embeds: [embed]});
-        return;
-    }
-
+	name: string = 'nsfw';
+	registerData: ChatInputApplicationCommandData = {
+		name: this.name,
+		description: 'Check your current NSFW setting, or toggle it ON or OFF',
+		options: [
+			{
+				name: 'toggle',
+				description: 'Switch your servers NSFW status to ON or OFF',
+				type: ApplicationCommandOptionTypes.STRING,
+				required: false,
+				choices: choices,
+			},
+		],
+	};
+	requiredPermissions: bigint[] = [Permissions.FLAGS.SEND_MESSAGES];
+	async run(
+		bot: Bot,
+		interaction: CommandInteraction<CacheType>
+	): Promise<void> {
+		var setting = nsfw.ensure(interaction.guild!.id, 'off');
+		const embed = new MessageEmbed();
+		if (interaction.options.getString('toggle') == 'on') {
+			nsfw.set(interaction.guild!.id, 'on');
+			embed.setDescription('NSFW has been toggled `ON`');
+			interaction.reply({ embeds: [embed] });
+			return;
+		}
+		if (interaction.options.getString('toggle') == 'off') {
+			nsfw.set(interaction.guild!.id, 'off');
+			embed.setDescription('NSFW has been toggled `OFF`');
+			interaction.reply({ embeds: [embed] });
+			return;
+		}
+		embed.setDescription(
+			`Your NSFW settings are currently set to \`${setting.toUpperCase()}\``
+		);
+		interaction.reply({ embeds: [embed] });
+		return;
+	}
 }
