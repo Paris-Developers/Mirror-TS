c0203dcfdd683f063e9cb13412811d88d0ea7b0f
FordFriedel <ford.friedel@gmail.com>
2022-03-06 02:24:44 -0600

    update GuildCreate

--- src/events/GuildCreate.ts
+++ src/events/GuildCreate.ts
@@ -1,26 +1,30 @@
 import { EventHandler } from './EventHandler';
 import { Bot } from '../Bot';
-import { Guild, MessageEmbed, TextChannel, Permissions } from 'discord.js';
-import { permissionsCheck } from '../resources/permissionsCheck';
+import {
+	Guild,
+	MessageEmbed,
+	TextChannel,
+	Permissions,
+	GuildChannel,
+} from 'discord.js';
 
 export class GuildCreate implements EventHandler {
 	eventName = 'guildCreate';
 	async process(bot: Bot, guild: Guild): Promise<void> {
+		let embed = new MessageEmbed()
+			.setTitle('**:mirror: Mirror has arrived!**')
+			.setDescription(
+				'Thanks for inviting Mirror to your server! \n\n To get started with Mirror use **`/help`** for more information about commands and functionality\n\nUse **`/config`** to see what else you can do before Mirror is fully functional'
+			)
+			.setColor('#FFFFFF');
 		//Check Guild for channels
 		let channelList = await guild.channels.fetch();
-        for(let chan of channelList){
-            if (chan == TextChannel) {
-        }
 		(await channelList).forEach(async (channel) => {
-			if (channel.type == 'GUILD_TEXT') {
-				await channel.send('test');
+			if (channel.permissionsFor(guild.me!).has('SEND_MESSAGES')) {
+				console.log('New guild test');
+				let newChannel = channel as TextChannel;
+				return await newChannel.send({ embeds: [embed] });
 			}
 		});
-
-		//Find the first channel where discord can send embeds and messages
-
-		//send "welcome embed"
-
-		//if they do not have perms to embed, send plain text
 	}
 }
