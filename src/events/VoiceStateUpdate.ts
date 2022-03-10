731c3bb846db5596f68d20039c1702a7601598ea
FordFriedel <ford.friedel@gmail.com>
2022-03-10 01:05:27 -0600

    change to VoiceStateUpdate

--- src/events/VoiceStateUpdate.ts
+++ src/events/VoiceStateUpdate.ts
@@ -7,6 +7,7 @@ import {
 	createAudioResource,
 } from '@discordjs/voice';
 import { VoiceState } from 'discord.js';
+import { player } from '../index';
 
 export class VoiceStateUpdate implements EventHandler {
 	eventName = 'voiceStateUpdate';
@@ -20,6 +21,7 @@ export class VoiceStateUpdate implements EventHandler {
 		if (newState.channelId != ourId) return; //if the new channel of the user doesnt match mirrors, end
 		if (oldState.channelId == newState.channelId) return; //if the new channel and the old channel are the same, end
 		if (newState.serverMute == true || newState.serverDeaf == true) return; //if the user is server muted or server deafened, end
+		if (player.getQueue(newState.guild)) return console.log('Test!'); //if there is a player queue available, end TODO? we may want to remove this later
 		let connection = getVoiceConnection(newState.guild.id);
 		if (!connection) {
 			connection = joinVoiceChannel({
