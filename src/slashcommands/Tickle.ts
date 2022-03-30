7e87776ff94615c7e0512293c8dc2bcbd163da16
SpookyLukie <71572783+SpookyLukie@users.noreply.github.com>
2022-03-30 13:43:20 -0700

    tickle commit

--- src/slashcommands/Tickle.ts
+++ src/slashcommands/Tickle.ts
@@ -1,13 +1,27 @@
-import { ApplicationCommandDataResolvable, CommandInteraction, CacheType, Permissions, MessageEmbed} from "discord.js";
+//Call: Slash command Tickle
+// Returns a tickle
+import { ApplicationCommandDataResolvable,
+    CommandInteraction,
+    CacheType,
+    Permissions,
+    MessageEmbed,
+    Application,
+    ApplicationCommand
+} from "discord.js";
 import { Bot } from "../Bot";
 import { SlashCommand } from "./SlashCommand";
 import fetch from "node-fetch";
+import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
 
 export class Tickle implements SlashCommand {
     name: string = 'tickle';
     registerData: ApplicationCommandDataResolvable = {
         name: this.name,
         description: 'Get tickled',
+        options: [{name: 'user',
+        description: 'Tickle a user',
+        type: ApplicationCommandOptionTypes.USER,
+        required: false}]
     };
     requiredPermissions: bigint[] = [
 		Permissions.FLAGS.SEND_MESSAGES,
@@ -21,6 +35,10 @@ export class Tickle implements SlashCommand {
         let jsonData = await res.json();
         console.log(jsonData);
         let tickleEmbed = new MessageEmbed().setColor('RANDOM').setImage(jsonData.url).setTitle(jsonData.anime_name);
+        if (interaction.options.getUser('user')) {
+            tickleEmbed.setDescription(`${interaction.user} tickled ${interaction.options.getUser('user')}`);
+        }
+        
         interaction.reply({embeds: [tickleEmbed] });
         
 
