c0c2509c5e85c86a8edfa3c59bda8f95c3984750
FordFriedel <ford.friedel@gmail.com>
2022-04-07 10:30:08 -0500

    maybe this is working

--- src/slashcommands/Profile.ts
+++ src/slashcommands/Profile.ts
@@ -3,6 +3,7 @@ import { Bot } from "../Bot";
 import { colorCheck } from "../resources/embedColorCheck";
 import { Option, Subcommand } from "./Option";
 import { SlashCommand } from "./SlashCommand";
+import { experience } from "../resources/experienceAdd";
 
 export class Profile implements SlashCommand {
     name: string = 'profile';
@@ -12,10 +13,39 @@ export class Profile implements SlashCommand {
     run(bot: Bot, interaction: CommandInteraction<CacheType>): Promise<void> {
         try{
             let user = interaction.user;
+            let list = [];
+            for(let rank of experience){
+                list.push(rank);
+            }
+            var propRetrive = function(obj: { xp: number; }[]) {
+                return obj[1].xp;
+            }
+
+            var sort = function(propertyRetriever : any, arr : any[]) {
+                arr.sort(function (a,b) {
+                    var valueA = propertyRetriever(a);
+                    var valueB = propertyRetriever(b);
+                    if (valueA < valueB) {
+                        return -1;
+                    } 
+                    if (valueA > valueB) {
+                        return 1;
+                    }
+                    return 0;
+                })
+                return arr;
+            }
+            list = sort(propRetrive,list);
+            console.log(list);
+            
+
+
+            console.log(user.avatarURL());
             const embed = new MessageEmbed()
                 .setTitle(`Mirror profile for ${user.username}`)
-                .setColor(colorCheck(interaction.guild!.id))
-                .setImage(user.avatar ? user.avatar : user.defaultAvatarURL);
+                .setColor(colorCheck(interaction.guild!.id));
+            if(user.avatarURL()) embed.setThumbnail(user.avatarURL()!);
+            else embed.setThumbnail(user.defaultAvatarURL);
             return interaction.reply({embeds: [embed]});
         } catch (err) {
 			bot.logger.commandError(interaction.channel!.id, this.name, err);
