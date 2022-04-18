7e08b7962f55b85f74a625db490b128e2231fc2d
FordFriedel <ford.friedel@gmail.com>
2022-04-18 08:27:03 -0500

    I think this test will fail!

--- tests/unit-tests/test.ts
+++ tests/unit-tests/test.ts
@@ -1,7 +1,10 @@
 // const jest = require('ts-jest');
 import 'ts-jest';
+import {colorCheck} from '../../src/resources/embedColorCheck'
 
-
+test('check that the color of the development guild is RANDOM', () => {
+    expect(colorCheck('938519232155648011',false)).toBe('RANDOM');
+});
 
 
 test('adds 1 + 2 to equal 3', () => {
