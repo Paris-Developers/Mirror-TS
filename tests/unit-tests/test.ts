42bfae0ad8c910f83fbdd837ef77bb43f48007f9
FordFriedel <ford.friedel@gmail.com>
2022-04-18 08:31:48 -0500

    I believe this test will pass!

--- tests/unit-tests/test.ts
+++ tests/unit-tests/test.ts
@@ -1,11 +1,13 @@
-// const jest = require('ts-jest');
 import 'ts-jest';
 import {colorCheck} from '../../src/resources/embedColorCheck'
 
-test('check that the color of the development guild is RANDOM', () => {
-    expect(colorCheck('938519232155648011',false)).toBe('RANDOM');
+test('check that the color of a default command is #FFFFFF', () => {
+    expect(colorCheck('938519232155648011',false)).toBe('#FFFFFF');
 });
 
+test('check that the color of a music command is BLUE', () => {
+    expect(colorCheck('938519232155648011',true)).toBe('#BLUE');
+});
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
