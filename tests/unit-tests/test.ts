edcbed2fb70786f33ecabd563cdb1155a2ecd9c8
FordFriedel <ford.friedel@gmail.com>
2022-04-20 11:42:23 -0500

    broken commit

--- tests/unit-tests/test.ts
+++ tests/unit-tests/test.ts
@@ -2,11 +2,11 @@ import 'ts-jest';
 import {colorCheck} from '../../src/resources/embedColorCheck'
 
 test('check that the color of a default command is #FFFFFF', () => {
-    expect(colorCheck('938519232155648011',false)).toBe('#FFFFFF');
+    expect(colorCheck('',false)).toBe('#FFFFFF');
 });
 
 test('check that the color of a music command is BLUE', () => {
-    expect(colorCheck('938519232155648011',true)).toBe('BLUE');
+    expect(colorCheck('',true)).toBe('BLUE');
 });
 
 test('adds 1 + 2 to equal 3', () => {
