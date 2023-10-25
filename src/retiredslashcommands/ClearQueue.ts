3924b3e335d95752b9864ab8a3bbc08880dbc6c3
FordFriedel <ford.friedel@gmail.com>
2023-10-25 09:58:12 -0500

    remove music commands

---
+++ src/retiredslashcommands/ClearQueue.ts
@@ -0,0 +1,43 @@
+import {
+	ApplicationCommandDataResolvable,
+	CommandInteraction,
+	CacheType,
+	MessageEmbed,
+	GuildMember,
+} from 'discord.js';
+import { Bot } from '../Bot';
+import { colorCheck } from '../resources/embedColorCheck';
+import { SlashCommand } from './SlashCommand';
+
+export class ClearQueue implements SlashCommand {
+	name: string = 'clearqueue';
+	description = 'Clear the music queue';
+	options = [];
+	requiredPermissions: bigint[] = [];
+	run(bot: Bot, interaction: CommandInteraction<CacheType>): Promise<void> {
+		try {
+			const embed = new MessageEmbed().setColor(colorCheck(interaction.guild!.id,true));
+
+			let queue = bot.player.getQueue(interaction.guild!.id);
+			if (!queue || !queue.playing) {
+				embed.setDescription(
+					'There are no songs in the queue or the player is not playing'
+				);
+				return interaction.reply({ embeds: [embed], ephemeral: true });
+			}
+			queue.clear();
+			embed.setDescription(`Queue has been cleared by ${interaction.user}`);
+			return interaction.reply({ embeds: [embed] });
+		} catch (err) {
+			bot.logger.commandError(interaction.channel!.id, this.name, err);
+			return interaction.reply({
+				content: 'Error: contact a developer to investigate',
+				ephemeral: true,
+			});
+		}
+	}
+	guildRequired?: boolean | undefined = true;
+	managerRequired?: boolean | undefined;
+	blockSilenced?: boolean | undefined = true;
+	musicCommand?: boolean | undefined = true;
+}
---
+++ src/retiredslashcommands/Resume.ts
@@ -0,0 +1,45 @@
+import {
+	ApplicationCommandDataResolvable,
+	CommandInteraction,
+	CacheType,
+	MessageEmbed,
+	GuildMember,
+} from 'discord.js';
+import { Bot } from '../Bot';
+import { colorCheck } from '../resources/embedColorCheck';
+import { SlashCommand } from './SlashCommand';
+
+export class Resume implements SlashCommand {
+	name: string = 'resume';
+	description = 'Resume the music player';
+	options = [];
+	requiredPermissions: bigint[] = [];
+	async run(
+		bot: Bot,
+		interaction: CommandInteraction<CacheType>
+	): Promise<void> {
+		try {
+			const embed = new MessageEmbed().setColor(colorCheck(interaction.guild!.id,true));
+
+			let queue = bot.player.getQueue(interaction.guild!.id);
+			if (!queue || !queue.playing) {
+				embed.setDescription('There is no music playing!');
+				return interaction.reply({ embeds: [embed], ephemeral: true });
+			}
+			queue.setPaused(false);
+			embed.setDescription(`Track was resumed by ${interaction.user}`);
+			return interaction.reply({ embeds: [embed] });
+		} catch (err) {
+			bot.logger.commandError(interaction.channel!.id, this.name, err);
+			interaction.reply({
+				content: 'Error: contact a developer to investigate',
+				ephemeral: true,
+			});
+			return;
+		}
+	}
+	guildRequired?: boolean | undefined = true;
+	managerRequired?: boolean | undefined;
+	blockSilenced?: boolean | undefined = true;
+	musicCommand?: boolean | undefined = true;
+}
